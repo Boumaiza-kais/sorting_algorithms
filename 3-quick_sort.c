2dc81bf0f81b42b8c747eafd39f0ee5a7e0c313e
boumaiza-kais <1110@holbertonschool.com>
2020-02-05 11:22:25 +0100

    fix betty

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,7 @@
 #include "sort.h"
+
 /**
- * partition_quicksor - finds the partition and swap
+ * partition_quicksort - finds the partition and swap
  * @array: array to sort
  * @lo: lowest index of the partition to sort
  * @up: highest index of the partition to sort
@@ -10,45 +11,44 @@
 
 void partition_quicksort(int *array, int lo, int up, size_t size)
 {
-	int j;
-	int piv;
-	int i = lo - 1;
+int j;
+int piv;
+int i = lo - 1;
 
-	for (j = lo; j < up; j++)
-	{
-		if ((array[j] < array[up]) )
-		{
-			i++;
-			piv = array[j];
-			array[j] = array[i];
-			array[i] = piv;
-			if (i != j)
-				print_array(array, size);
-		}
-	}
-	i++;
-	if (array[up] != array[i])
-	{
-		piv = array[up];
-		array[up] = array[i];
-		array[i] = piv;
-		if (i != up)
-			print_array(array, size);
-	}
-	if (lo < i - 1)
-	        partition_quicksort(array, lo, i - 1, size);
-	if (i + 1 < up)
-	        partition_quicksort(array, i + 1, up, size);
-
+for (j = lo; j < up; j++)
+{
+if (array[j] < array[up])
+{
+i++;
+piv = array[j];
+array[j] = array[i];
+array[i] = piv;
+if (i != j)
+print_array(array, size);
 }
-
+}
+i++;
+if (array[up] != array[i])
+{
+piv = array[up];
+array[up] = array[i];
+array[i] = piv;
+if (i != up)
+print_array(array, size);
+}
+if (lo < i - 1)
+partition_quicksort(array, lo, i - 1, size);
+if (i + 1 < up)
+partition_quicksort(array, i + 1, up, size);
+}
 
 /**
  * quick_sort - quick sorting
  * @array: array
  * @size: size
  */
+
 void quick_sort(int *array, size_t size)
 {
-        partition_quicksort(array, 0 , size - 1, size);
+partition_quicksort(array, 0, size - 1, size);
 }

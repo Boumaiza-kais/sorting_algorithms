9810dd72ef79837fe522c04783ebd25b8c90c763
boumaiza-kais <1110@holbertonschool.com>
2020-02-05 11:51:36 +0100

    quick_sort fix

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,7 +1,45 @@
 #include "sort.h"
 
 /**
- * partition_quicksort - finds the partition and swap
+ * partition_function - finds the partition
+ * @array: array to sort
+ * @lo: lowest index of the partition to sort
+ * @up: highest index of the partition to sort
+ * @size: size of the array
+ * Return: nothing
+ */
+int partition_function(int *array, int lo, int up, size_t size)
+{
+	int i = lo - 1;
+	int pivot, j;
+
+	for (j = lo; j <= up - 1; j++)
+	{
+		if (array[j] < array[up])
+		{
+			i++;
+			if (i < j)
+			{
+				pivot = array[i];
+				array[i] = array[j];
+				array[j] = pivot;
+				print_array(array, size);
+			}
+		}
+	}
+	if (array[i + 1] > array[up])
+	{
+		pivot = array[i + 1];
+		array[i + 1] = array[up];
+		array[up] = pivot;
+		print_array(array, size);
+	}
+
+	return (i + 1);
+}
+
+/**
+ * call_fun - sorts a partition of an array
  * @array: array to sort
  * @lo: lowest index of the partition to sort
  * @up: highest index of the partition to sort
@@ -9,46 +47,26 @@
  * Return: nothing
  */
 
-void partition_quicksort(int *array, int lo, int up, size_t size)
+void call_fun(int *array, int lo, int up, size_t size)
 {
-int j;
 int piv;
-int i = lo - 1;
 
-for (j = lo; j < up; j++)
+if (lo < up)
 {
-if (array[j] < array[up])
-{
-i++;
-piv = array[j];
-array[j] = array[i];
-array[i] = piv;
-if (i != j)
-print_array(array, size);
+piv = partition_function(array, lo, up, size);
+call_fun(array, lo, piv - 1, size);
+call_fun(array, piv + 1, up, size);
 }
-}
-i++;
-if (array[up] != array[i])
-{
-piv = array[up];
-array[up] = array[i];
-array[i] = piv;
-if (i != up)
-print_array(array, size);
-}
-if (lo < i - 1)
-partition_quicksort(array, lo, i - 1, size);
-if (i + 1 < up)
-partition_quicksort(array, i + 1, up, size);
 }
 
 /**
- * quick_sort - quick sorting
- * @array: array
- * @size: size
+ * quick_sort - sorts an array of integers in ascending
+ * @array: The array to sort
+ * @size: The size of the array
+ * Return: nothing
  */
 
 void quick_sort(int *array, size_t size)
 {
-partition_quicksort(array, 0, size - 1, size);
+	call_fun(array, 0, size - 1, size);
 }
